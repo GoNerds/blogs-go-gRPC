4c9d37398400770e155b3451223fe0f3028418bf
Robert Stevens <robert.stevens86091@gmail.com>
2024-07-21 21:10:51 -0400

    grpc server added

--- internal/grpc/server.go
+++ internal/grpc/server.go
@@ -1,1 +1,23 @@
-ECHO is on.
+package grpc
+
+import (
+	"context"
+
+	pb "blogs-go-grpc/internal/grpc/proto"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type BlogServer struct {
+	pb.UnimplementedBlogServiceServer
+	dbClient *mongo.Client
+}
+
+func NewBlogServer(dbClient *mongo.Client) *BlogServer {
+	return &BlogServer{dbClient: dbClient}
+}
+
+func (s *BlogServer) CreateBlog(ctx context.Context, req *pb.CreateBlogRequest) (*pb.CreateBlogResponse, error) {
+	return &pb.CreateBlogResponse{}, nil
+}
+
