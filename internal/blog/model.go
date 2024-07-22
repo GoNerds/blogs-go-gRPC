f539278086b4280355c7f6705b91bb3d8d19243c
Robert Stevens <robert.stevens86091@gmail.com>
2024-07-21 21:39:34 -0400

    blog model added

--- internal/blog/model.go
+++ internal/blog/model.go
@@ -1,1 +1,8 @@
-ECHO is on.
+package blog
+
+type Blog struct {
+	ID      string `bson:"_id, omitempty"`
+	Title   string `bson:"title"`
+	Content string `bson:"content"`
+	Author  string `bson:"author"`
+}
