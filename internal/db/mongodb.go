723fd01f92184417d80026838231504360c71f93
Robert Stevens <robert.stevens86091@gmail.com>
2024-07-21 20:59:52 -0400

    mongodb connection file added

--- internal/db/mongodb.go
+++ internal/db/mongodb.go
@@ -1,1 +1,25 @@
-ECHO is on.
+package db
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Connect(uri string) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
